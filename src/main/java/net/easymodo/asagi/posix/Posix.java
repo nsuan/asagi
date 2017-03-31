8acf4bb1192cda95687a98fe7297b4408dd9ad38
Nick Suan <nsuan@nonexiste.net>
2017-03-31 09:03:10 -0400

    Finish removing jna

--- src/main/java/net/easymodo/asagi/posix/Posix.java
+++
@@ -1,9 +0,0 @@
-package net.easymodo.asagi.posix;
-
-import com.sun.jna.Library;
-
-public interface Posix extends Library {
-    int chmod(String path, int mode);
-    int chown(String filename, int owner, int group);
-    Group getgrnam(String groupName);
-}
